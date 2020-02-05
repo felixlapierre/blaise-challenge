e523804644339d2d591b3b0592588bf42aaf6a4b
Felix <felix.lapierre1@gmail.com>
2020-02-05 11:54:31 -0500

    Map now displays & JSON files are imported

--- transit/src/App.js
+++ transit/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import ReactMapGL from 'react-map-gl';
+
+var passengers = require("./passengers.json");
+var stops = require("./stops.json");
+
+const mapboxAccessToken = "pk.eyJ1IjoiZmVsaXhsYXBpZXJyZSIsImEiOiJjazY5aXJ3bGswZzdrM2ZtcmY1cGxxeDNpIn0.WYhzLFieiQHNbp4LKyGWyw";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [viewPort, setViewport] = useState({
+        width: 800,
+        height: 600,
+        latitude: 45.5129604703,
+        longitude: -73.5729924601,
+        zoom: 12
+    })
+
+    return (<ReactMapGL {...viewPort}
+        onViewportChange={setViewport}
+        mapboxApiAccessToken={mapboxAccessToken}/>);
 }
 
 export default App;
