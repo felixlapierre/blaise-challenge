0923b6f4079feac9f1bab47d8bc13d5fd308ce2e
Felix <felix.lapierre1@gmail.com>
2020-02-06 00:03:25 -0500

    Converted to class

--- transit/src/App.js
+++ transit/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import './App.css';
-import ReactMapGL, { Source, Layer, Marker } from 'react-map-gl';
+import ReactMapGL, { Source, Layer, Marker, Popup } from 'react-map-gl';
 import { createPassengerMarker, createBusStopMarker } from './images/marker';
 import Polyline from './Polyline';
 
@@ -9,8 +9,115 @@ var stops = require("./stops.json");
 
 const mapboxAccessToken = "pk.eyJ1IjoiZmVsaXhsYXBpZXJyZSIsImEiOiJjazY5aXJ3bGswZzdrM2ZtcmY1cGxxeDNpIn0.WYhzLFieiQHNbp4LKyGWyw";
 
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                width: 800,
+                height: 600,
+                latitude: 45.5129604703,
+                longitude: -73.5729924601,
+                zoom: 15
+            },
+            popup: null
+        };
+    }
+
+    pythagoras(x1, y1, x2, y2) {
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    }
+
+    renderPopup() {
+        var { popup } = this.state;
+
+        return popup &&
+            (<Popup
+                tipSize={5}
+                anchor="top"
+                longitude={popup.lon}
+                latitude={popup.lat}
+                closeOnClick={false}
+                onClose={() => this.setState({ popupInfo: null })}
+            >
+                Stop {popup.id} passengers {popup.count}
+            </Popup>)
+    }
+
+
+    render() {
+
+        const markers = [];
+        const lines = [];
+        const stopPassengerCounts = {};
+
+        passengers.forEach(passenger => {
+            //Find the nearest bus stop
+            let minDistance = Number.MAX_VALUE;
+            let closestStop = stops[0];
+            let closestStopIndex = 0;
+            stops.forEach((stop, i) => {
+                const distance = this.pythagoras(passenger.lon, passenger.lat, stop.lon, stop.lat);
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    closestStop = stop;
+                    closestStopIndex = i;
+                }
+            })
+            const point1 = [passenger.lat, passenger.lon];
+            const point2 = [closestStop.lat, closestStop.lon];
+
+            lines.push(<Polyline points={[point1, point2]} />)
+
+            if (stopPassengerCounts[closestStopIndex]) {
+                stopPassengerCounts[closestStopIndex] += 1;
+            } else {
+                stopPassengerCounts[closestStopIndex] = 1;
+            }
+
+            markers.push(
+                <Marker latitude={passenger.lat}
+                    longitude={passenger.lon}>{createPassengerMarker()}</Marker>)
+        });
+        stops.forEach((stop, i) => {
+            markers.push(
+                <div onMouseEnter={() => this.state.popup = { lat: stop.lat, lon: stop.lon, id: i, count: stopPassengerCounts[i] }}
+                    onMouseLeave={() => this.state.popup = null}>
+                    <Marker latitude={stop.lat}
+                        longitude={stop.lon}>{createBusStopMarker()} {stopPassengerCounts[i]}</Marker>
+                </div>
+            )
+        })
+
+        return (<ReactMapGL {...this.state.viewport}
+            onViewportChange={viewport => this.setState({ viewport })}
+            mapboxApiAccessToken={mapboxAccessToken}
+        >
+            {lines}
+            {markers}
+            {this.renderPopup()}
+        </ReactMapGL>);
+    }
+}
+/*
 function pythagoras(x1, y1, x2, y2) {
     return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+
+function renderPopup() {
+    var { popup } = this.state;
+
+    return popup &&
+        (<Popup
+            tipSize={5}
+            anchor="top"
+            longitude={popup.lon}
+            latitude={popup.lat}
+            closeOnClick={false}
+            onClose={() => this.setState({ popupInfo: null })}
+        >
+            Stop {popup.id} passengers {popup.count}
+        </Popup>)
 }
 
 function App() {
@@ -19,39 +126,49 @@ function App() {
         height: 600,
         latitude: 45.5129604703,
         longitude: -73.5729924601,
-        zoom: 15
+        zoom: 15,
+        popup: null
     })
-
-    let person1 = [passengers[0].lat, passengers[0].lon];
-    let person2 = [passengers[1].lat, passengers[1].lon];
 
     const markers = [];
     const lines = [];
+    const stopPassengerCounts = {};
+
     passengers.forEach(passenger => {
         //Find the nearest bus stop
         let minDistance = Number.MAX_VALUE;
         let closestStop = stops[0];
-        stops.forEach((stop) => {
+        let closestStopIndex = 0;
+        stops.forEach((stop, i) => {
             const distance = pythagoras(passenger.lon, passenger.lat, stop.lon, stop.lat);
             if (distance < minDistance) {
                 minDistance = distance;
                 closestStop = stop;
+                closestStopIndex = i;
             }
         })
         const point1 = [passenger.lat, passenger.lon];
         const point2 = [closestStop.lat, closestStop.lon];
 
-        lines.push(<Polyline points={[point1, point2]}/>)
-        console.log(closestStop);
+        lines.push(<Polyline points={[point1, point2]} />)
+
+        if (stopPassengerCounts[closestStopIndex]) {
+            stopPassengerCounts[closestStopIndex] += 1;
+        } else {
+            stopPassengerCounts[closestStopIndex] = 1;
+        }
 
         markers.push(
             <Marker latitude={passenger.lat}
                 longitude={passenger.lon}>{createPassengerMarker()}</Marker>)
     });
-    stops.forEach(stop => {
+    stops.forEach((stop, i) => {
         markers.push(
-            <Marker latitude={stop.lat}
-                longitude={stop.lon}>{createBusStopMarker()}</Marker>
+            <div onMouseEnter={() => this.state.popup = { lat: stop.lat, lon: stop.lon, id: i, count: stopPassengerCounts[i] }}
+                onMouseLeave={() => this.state.popup = null}>
+                <Marker latitude={stop.lat}
+                    longitude={stop.lon}>{createBusStopMarker()} {stopPassengerCounts[i]}</Marker>
+            </div>
         )
     })
 
@@ -61,7 +178,9 @@ function App() {
     >
         {lines}
         {markers}
+        {renderPopup()}
     </ReactMapGL>);
 }
 
 export default App;
+*/
